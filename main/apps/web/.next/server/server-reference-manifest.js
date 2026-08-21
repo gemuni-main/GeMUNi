0bf6106034eb38faac00de088a447d12bb19193c
gemuni-main <gemunimain@gmail.com>
2026-08-21 21:07:31 +0400

    Delete main directory

--- main/apps/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sO4aqJY+GgAB7gInKMIktO5Cd9g01GIHTZ16J2QaKyI=\"\n}"
--- main/apps/web/src/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"M5VBX2jtDFIXUtSkOqMrhPauMM6d1Lqy7l1CcU7da2Y=\"\n}"
